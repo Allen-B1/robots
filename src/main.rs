593f16d362366d14803ec4022d7fec06641f0178
Allen B <pingguo.ny@gmail.com>
2023-05-19 16:34:49 -0400

    move list...

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ use leptos::*;
 use leptos::ev::KeyboardEvent;
 mod rand;
 
+#[derive(Clone, Copy)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+impl Direction {
+    pub fn id(self) -> &'static str {
+        match self {
+            Direction::Up => "up",
+            Direction::Down => "down",
+            Direction::Left => "left",
+            Direction::Right => "right"
+        }
+    }
+}
 type RobotPositions = [usize; 5];
 
 #[derive(Clone)]
@@ -104,65 +121,31 @@ impl Board {
         self.width / 2 - 1 <= x && x <= self.width / 2 &&
             height / 2 - 1 <= y && y <= height / 2
     }
-}
 
-#[component]
-pub fn SimpleCounter(cx: Scope, initial_value: i32) -> impl IntoView {
-    // create a reactive signal with the initial value
-    let (value, set_value) = create_signal(cx, initial_value);
-
-    // create event handlers for our buttons
-    // note that `value` and `set_value` are `Copy`, so it's super easy to move them into closures
-    let clear = move |_| set_value.set(0);
-    let decrement = move |_| set_value.update(|value| *value -= 1);
-    let increment = move |_| set_value.update(|value| *value += 1);
-
-    // this JSX is compiled to an HTML template string for performance
-    view! {
-        cx,
-        <div>
-            <button on:click=clear>"Clear"</button>
-            <button on:click=decrement>"-1"</button>
-            <span>"Value: " {move || value().to_string()} "!"</span>
-            <button on:click=increment>"+1"</button>
-        </div>
-    }
-}
-
-#[component]
-pub fn BoardWidget(cx: Scope, board: ReadSignal<Board>, positions: Option<(ReadSignal<RobotPositions>, WriteSignal<RobotPositions>)>) -> impl IntoView {
-    let (positions, set_positions) = match positions {
-        None => (Signal::derive(cx, move || board().initial_positions), None),
-        Some((a, b)) => (a.into(), Some(b))
-    };
-
-    let keydown = move |robot, evt: KeyboardEvent| {
-        log!("{}", evt.code());
-
-        let mut positions = positions();
-        let horizontal_walls = board().horizontal_walls;
-        let vertical_walls = board().vertical_walls;
-        let width = board().width;
-
-        match evt.code().as_str() {
-            "ArrowDown" => {
-                let wall_pos = horizontal_walls.iter().enumerate()
+    /// Given a robot position, move a robot in
+    /// a direction. Returns the new robot positions.
+    pub fn move_robot(&self, mut positions: RobotPositions, robot: usize, direction: Direction) -> RobotPositions {
+        let width = self.width;
+        let height = self.height();
+        match direction {
+            Direction::Down => {
+                let wall_pos = self.horizontal_walls.iter().enumerate()
                     .filter(|(i, b)| (**b) && (*i % width  == positions[robot] % width))
                     .map(|(i, _)| i / width)
                     .filter(|&i| i + 1 > positions[robot] / width)
-                    .next().unwrap_or(board().height() - 1);
+                    .next().unwrap_or(height - 1);
                 let robot_pos = positions.iter()
                     .filter(|&i| i % width == positions[robot] % width)
                     .map(|i| i / width)
                     .filter(|&i| i > positions[robot] / width)
-                    .min().map(|i| i - 1).unwrap_or(board().height() - 1);
+                    .min().map(|i| i - 1).unwrap_or(height - 1);
                 let target_pos = usize::min(wall_pos, robot_pos);
 
                 positions[robot] = positions[robot] % width + target_pos * width;
-                set_positions.unwrap().set(positions);
+                positions   
             },
-            "ArrowUp" => {
-                let wall_pos = horizontal_walls.iter().enumerate().rev()
+            Direction::Up => {
+                let wall_pos = self.horizontal_walls.iter().enumerate().rev()
                     .filter(|(i, b)| (**b) && (*i % width  == positions[robot] % width))
                     .map(|(i, _)| i / width)
                     .filter(|&i| i < positions[robot] / width)
@@ -175,26 +158,26 @@ pub fn BoardWidget(cx: Scope, board: ReadSignal<Board>, positions: Option<(ReadS
                 let target_pos = usize::max(wall_pos, robot_pos);
 
                 positions[robot] = positions[robot] % width + target_pos * width;
-                set_positions.unwrap().set(positions);
+                positions
             },
-            "ArrowRight" => {
-                let wall_pos = vertical_walls.iter().enumerate()
+            Direction::Right => {
+                let wall_pos = self.vertical_walls.iter().enumerate()
                     .filter(|&(i, b)| (*b && i / (width - 1) == positions[robot] / width))
                     .map(|(i, _)| i % (width - 1))
                     .filter(|&i| i + 1 > positions[robot] % width)
-                    .next().map(|i| i).unwrap_or(board().width - 1);
+                    .next().map(|i| i).unwrap_or(width - 1);
                 let robot_pos = positions.iter()
                     .filter(|&i| i / width == positions[robot] / width)
                     .map(|i| i % width)
                     .filter(|&i| i > positions[robot] % width)
-                    .min().map(|i| i - 1).unwrap_or(board().width - 1);
+                    .min().map(|i| i - 1).unwrap_or(width - 1);
                 let target_pos = usize::min(wall_pos, robot_pos);
 
                 positions[robot] = (positions[robot] / width) * width + target_pos;
-                set_positions.unwrap().set(positions);
+                positions
             },
-            "ArrowLeft" => {
-                let wall_pos = vertical_walls.iter().enumerate().rev()
+            Direction::Left => {
+                let wall_pos = self.vertical_walls.iter().enumerate().rev()
                     .filter(|&(i, b)| *b && i / (width - 1) == positions[robot] / width)
                     .map(|(i, _)| i % (width - 1))
                     .filter(|&i| i < positions[robot] % width)
@@ -207,15 +190,47 @@ pub fn BoardWidget(cx: Scope, board: ReadSignal<Board>, positions: Option<(ReadS
                 let target_pos = usize::max(wall_pos, robot_pos);
 
                 positions[robot] = (positions[robot] / width) * width + target_pos;
-                set_positions.unwrap().set(positions);
+                positions
             }
-            _ => {}
+        }
+    }
+}
+
+#[component]
+pub fn BoardWidget(cx: Scope, board: ReadSignal<Board>, positions: Option<RwSignal<RobotPositions>>, moves: RwSignal<Vec<(usize, Direction)>>) -> impl IntoView {
+    // invariant: if set_position is None, moves is empty
+    let (positions, set_positions) = match positions {
+        None => (Signal::derive(cx, move || board().initial_positions), None),
+        Some(rw) => (rw.into(), Some(rw.write_only()))
+    };
+
+    let keydown = move |robot, evt: KeyboardEvent| {
+        let positions = positions();
+
+        let dir = match evt.code().as_str() {
+            "ArrowDown" => Direction::Down,
+            "ArrowUp" => Direction::Up,
+            "ArrowRight" => Direction::Right,
+            "ArrowLeft" => Direction::Left,
+            _ => {return}
+        };
+
+        let new_positions = board().move_robot(positions, robot, dir);
+        if new_positions != positions {
+            set_positions.unwrap().set(new_positions);
+            moves.update(|moves| moves.push((robot, dir)));
         }
     };
 
     view !{
         cx, 
         <div class="board" style={format!("width:{}px;height:{}px", 32 * board.get().width, 32 * board.get().height())}>
+            {move || if moves.get().len() != 0 {
+                Some(view!{ cx, <div class="refresh" on:click={move |_| {
+                    moves.update(|v| v.clear());
+                    set_positions.unwrap().set(board().initial_positions);
+                }}></div> })
+            } else { None }}
             <For 
                 each=move || 0..board.get().width*board.get().height()
                 key=|&i| i
@@ -285,10 +300,29 @@ pub fn BoardWidget(cx: Scope, board: ReadSignal<Board>, positions: Option<(ReadS
     }
 }
 
+#[component]
+pub fn MoveList(cx: Scope, moves: ReadSignal<Vec<(usize, Direction)>>) -> impl IntoView {
+
+    view! { cx,
+        <div class="move-list">
+            <For
+                each={move || moves().into_iter().enumerate().collect::<Vec<_>>()}
+                key={|&(i, _)| i}
+                view=move |cx, (_, (robot, dir))| {
+                    view! {cx, <span class={format!("move move-{} move-{}", robot, dir.id())}></span>}
+                }
+                />
+        </div>
+    }
+}
+
 pub fn main() {
     mount_to_body(|cx| {
         let (board, set_board) = create_signal(cx, Board::generate(16, 16));
-        let (positions, set_positions) = create_signal(cx, board().initial_positions);
-        view! { cx,  <BoardWidget board={board} positions={Some((positions, set_positions))} /> }
+        let positions = create_rw_signal(cx, board().initial_positions);
+        let moves = create_rw_signal(cx, Vec::new());
+        view! { cx,  
+            <BoardWidget board={board} positions={Some(positions)} moves={moves} />
+            <MoveList moves={moves.read_only()} /> }
     })
 }
